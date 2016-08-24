80f4827239a8d0ca83e131622f14dc3e6c5cda43
John Krug <j.krug@lancaster.ac.uk>
2016-08-24 12:54:39 +0100

    Added DmaoAdmin model, routes, and authentication routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
 
   root to: 'pages#main'
 
+  scope :admin do
+    devise_for :dmao_admins, path: '', path_names: {sign_in: 'login', sign_out: 'logout', password: 'reset_password', unlock: 'unlock'}
+  end
+
   namespace :admin do
     resources :institutions
   end
