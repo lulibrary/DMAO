0c8964c0d2d63d445c204d89312c473e023d8d9f
John Krug <j.krug@lancaster.ac.uk>
2016-08-24 15:36:51 +0100

    Added DMAO Admin signin

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,17 @@ Rails.application.routes.draw do
   root to: 'pages#main'
 
   scope :admin do
-    devise_for :dmao_admins, path: '', path_names: {sign_in: 'login', sign_out: 'logout', password: 'reset_password', unlock: 'unlock'}
+    devise_for :dmao_admins,
+               path: '',
+               path_names: {
+                   sign_in: 'login',
+                   sign_out: 'logout',
+                   password: 'reset_password',
+                   unlock: 'unlock'
+               },
+               controllers: {
+                   sessions: 'admin/dmao_users/sessions'
+               }
   end
 
   namespace :admin do
