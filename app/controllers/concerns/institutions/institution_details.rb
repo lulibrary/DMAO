a7dd7fcba889e80f421715d0c76f80463a1fa751
Stephen Robinson <stephen@stephen-robinson.co.uk>
2016-09-09 11:53:35 +0100

    Removed byebug from institution details concern

--- app/controllers/concerns/institutions/institution_details.rb
+++ app/controllers/concerns/institutions/institution_details.rb
@@ -9,8 +9,6 @@ module Institutions
 
     def current_institution
 
-      # byebug
-
       begin
         @institution = Institution.find_by_identifier!(params[:institution_identifier])
       rescue ActiveRecord::RecordNotFound
