2204bfd12933bd64475cba4a1d40018d9e2f406b
Stephen Robinson <stephen@stephen-robinson.co.uk>
2016-09-09 12:17:55 +0100

    Added around action to set institution current id for scoping

--- app/controllers/concerns/institutions/institution_details.rb
+++ app/controllers/concerns/institutions/institution_details.rb
@@ -5,6 +5,7 @@ module Institutions
 
     included do
       before_action :current_institution
+      around_action :scope_current_institution
     end
 
     def current_institution
@@ -15,6 +16,13 @@ module Institutions
         head(:not_found)
       end
 
+    end
+
+    def scope_current_institution
+      Institution.current_id = @institution.id
+      yield
+    ensure
+     Institution.current_id = nil
     end
 
   end
--- test/concerns/institutions/institution_details_test.rb
+++ test/concerns/institutions/institution_details_test.rb
@@ -5,7 +5,7 @@ module Institutions
   class InstitutionDetailsTest < ActionController::TestCase
 
     def setup
-      @controller = InstitutionDetailsTestController.new(:test) { head(:ok) }
+      @controller = InstitutionDetailsTestController.new(:test) { render plain: Institution.current_id }
       @routes.draw { get 'test_route' => 'institutions/institution_details_test#test'}
       @existing_institution = institutions(:luve)
       @non_existing_institution = @existing_institution.dup
@@ -38,7 +38,17 @@ module Institutions
 
       assert assigns(:institution)
 
-      assert_equal assigns(:institution).identifier, @existing_institution.identifier
+      assert_equal @existing_institution.identifier, assigns(:institution).identifier
+
+    end
+
+    test 'should return the correct institution id from current id on institution based on institution requesting as' do
+
+      get :test, params: { institution_identifier: @existing_institution.identifier }
+
+      assert_response :ok
+
+      assert_equal @existing_institution.id.to_s, @response.body
 
     end
 
