504b1c7cbd48a28363b395ca2c115a10b70e05cd
Stephen Robinson <stephen@stephen-robinson.co.uk>
2016-09-13 14:56:50 +0100

    Added papertrail gem for storing configuration versions

--- Gemfile
+++ Gemfile
@@ -41,6 +41,9 @@ gem 'devise'
 # Form Fields
 gem 'nested_form_fields'
 
+# PaperTrail - versioning of model with log
+gem 'paper_trail'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platform: :mri
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160912153749) do
+ActiveRecord::Schema.define(version: 20160913135130) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -113,6 +113,16 @@ ActiveRecord::Schema.define(version: 20160912153749) do
     t.integer  "version"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "versions", force: :cascade do |t|
+    t.string   "item_type",  null: false
+    t.integer  "item_id",    null: false
+    t.string   "event",      null: false
+    t.string   "whodunnit"
+    t.text     "object"
+    t.datetime "created_at"
+    t.index ["item_type", "item_id"], name: "index_versions_on_item_type_and_item_id", using: :btree
   end
 
 end
