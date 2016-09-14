3d42cce9b786f586e3dbc5d4a47e6f1c9f8bc347
Stephen Robinson <stephen@stephen-robinson.co.uk>
2016-09-14 16:43:05 +0100

    Cleaned up javascript for new institution configuration

--- app/assets/javascripts/admin/institutions/configurations.js
+++ app/assets/javascripts/admin/institutions/configurations.js
@@ -1,17 +1,29 @@
 $(document).on("change", "select#institution_configuration_systems_configuration_cris_system_system_id", function(select) {
 
-    $.ajax({
-        url: "/admin/systems/cris_systems/" + $(this).val() + '/config_keys',
-        dataType: "json",
-        success: function(data) {
-            $.each(data, function(index, value) {
-                $("#cris-system-configuration").append(configKeyInput(value.id, value.name, value.display_name));
-            });
-        },
-        error: function(data){
+    $("#cris-system-configuration-values").empty();
 
-        }
-    });
+    if ($(this).val() !== "") {
+
+        loadConfigKeysForSystem($(this).val())
+
+    }
+
+    function loadConfigKeysForSystem(system_id) {
+
+        $.ajax({
+            url: "/admin/systems/cris_systems/" + system_id + '/config_keys',
+            dataType: "json",
+            success: function(data) {
+                $.each(data, function(index, value) {
+                    $("#cris-system-configuration-values").append(configKeyInput(value.id, value.name, value.display_name));
+                });
+            },
+            error: function(data){
+
+            }
+        });
+
+    }
 
     function configKeyInput(id, name, displayName) {
 
