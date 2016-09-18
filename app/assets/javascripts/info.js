854a6272037143afb752df978fd5fee49ccefffd
Stephen Robinson <stephen@stephen-robinson.co.uk>
2016-09-18 21:02:20 +0100

    Added javascript to disbaled login button unless institution selected

--- app/assets/javascripts/info.js
+++ app/assets/javascripts/info.js
@@ -2,3 +2,17 @@
 //= require jquery_ujs
 //= require bootstrap-sprockets
 //= require grayscale.min
+
+$(document).on("change", "select#institution_id", function(select) {
+
+    if ($(this).val() !== "") {
+
+        $("input#login-button").removeAttr("disabled");
+
+    } else {
+
+        $("input#login-button").attr("disabled", "disabled");
+
+    }
+
+});
