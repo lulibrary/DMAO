9471011de56b75a52b73b379afbd11140c18d071
Stephen Robinson <stephen@stephen-robinson.co.uk>
2016-09-19 11:54:52 +0100

    Added configuration key helper to return config key details in view

---
+++ app/helpers/configuration_key_helper.rb
@@ -0,0 +1,51 @@
+module ConfigurationKeyHelper
+
+  def configuration_key_id_name id
+
+    begin
+
+      key = Systems::ConfigurationKey.find(id)
+
+      key.display_name
+
+    rescue ActiveRecord::RecordNotFound
+
+      nil
+
+    end
+
+  end
+
+  def configuration_key_id_slug id
+
+    begin
+
+      key = Systems::ConfigurationKey.find(id)
+
+      key.name
+
+    rescue ActiveRecord::RecordNotFound
+
+      nil
+
+    end
+
+  end
+
+  def configuration_key_id_secure id
+
+    begin
+
+      key = Systems::ConfigurationKey.find(id)
+
+      false
+
+    rescue ActiveRecord::RecordNotFound
+
+      false
+
+    end
+
+  end
+
+end
