38d19eadd10bc4abb5eee04ddbd6b02b45ba2746
Stephen Robinson <stephen@stephen-robinson.co.uk>
2016-09-21 13:41:08 +0100

    Enabled uuid extension for db

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160919120806) do
+ActiveRecord::Schema.define(version: 20160921123849) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+  enable_extension "uuid-ossp"
 
   create_table "dmao_admins", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
