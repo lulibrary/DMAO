53aa079baeb6dce01875fb979cd043acd9261b2c
Stephen Robinson <stephen@stephen-robinson.co.uk>
2016-09-21 16:13:07 +0100

    Removed test from within modules

--- test/lib/dmao/ingesters/ingest_error_test.rb
+++ test/lib/dmao/ingesters/ingest_error_test.rb
@@ -1,48 +1,42 @@
 require 'test_helper'
 
-module DMAO
-  module Ingesters
+class IngestErrortest < ActiveSupport::TestCase
 
-    class IngestErrortest < ActiveSupport::TestCase
+  test "returns ingest error with message set to default when initialised empty" do
 
-      test "returns ingest error with message set to default when initialised empty" do
+    error = DMAO::Ingesters::IngestError.new
 
-        error = IngestError.new
-
-        assert_instance_of IngestError, error
-        assert_equal "Error ingesting object to system", error.message
-
-      end
-
-      test "return ingest error with validation errors set to nil when initialised empty" do
-
-        error = IngestError.new
-
-        assert_instance_of IngestError, error
-        assert_nil error.validation_errors
-
-      end
-
-      test 'returns ingest error with set message when calling initialise with message' do
-
-        error = IngestError.new ("Test error message")
-
-        assert_instance_of IngestError, error
-        assert_equal "Test error message", error.message
-
-      end
-
-      test 'returns ingest error with set message and validation errors when calling initialise with message and errors' do
-
-        error = IngestError.new("Test error message", "validation errors")
-
-        assert_instance_of IngestError, error
-        assert_equal "Test error message", error.message
-        assert_equal "validation errors", error.validation_errors
-
-      end
-
-    end
+    assert_instance_of DMAO::Ingesters::IngestError, error
+    assert_equal "Error ingesting object to system", error.message
 
   end
+
+  test "return ingest error with validation errors set to nil when initialised empty" do
+
+    error = DMAO::Ingesters::IngestError.new
+
+    assert_instance_of DMAO::Ingesters::IngestError, error
+    assert_nil error.validation_errors
+
+  end
+
+  test 'returns ingest error with set message when calling initialise with message' do
+
+    error = DMAO::Ingesters::IngestError.new ("Test error message")
+
+    assert_instance_of DMAO::Ingesters::IngestError, error
+    assert_equal "Test error message", error.message
+
+  end
+
+  test 'returns ingest error with set message and validation errors when calling initialise with message and errors' do
+
+    error = DMAO::Ingesters::IngestError.new("Test error message", "validation errors")
+
+    assert_instance_of DMAO::Ingesters::IngestError, error
+    assert_equal "Test error message", error.message
+    assert_equal "validation errors", error.validation_errors
+
+  end
+
 end
