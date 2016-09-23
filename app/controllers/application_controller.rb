97767d09733113f4c12d85e0a864775daae36576
Stephen Robinson <stephen@stephen-robinson.co.uk>
2016-09-23 12:21:18 +0100

    Added before actions to main extended controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  before_action :set_raven_context
+
+  private
+
+  def set_raven_context
+    Raven.extra_context(params: params.to_h, url: request.url)
+  end
+
 end
--- app/controllers/institutions/institutions_controller.rb
+++ app/controllers/institutions/institutions_controller.rb
@@ -5,6 +5,13 @@ module Institutions
     include Institutions::InstitutionDetails
 
     before_action :authenticate_institution_admin!
+    before_action :set_raven_institution_context
+
+    private
+
+    def set_raven_institution_context
+      Raven.user_context(id: current_institution_admin.id, type: "Institution Admin", institution_id: current_institution.id)
+    end
 
   end
 
