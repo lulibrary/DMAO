fd668fb315d98f52343011dedf796005de8819e5
Stephen Robinson <stephen@stephen-robinson.co.uk>
2016-09-26 11:22:47 +0100

    Add ingester type to ingesters register with nil default

--- app/lib/dmao/ingesters.rb
+++ app/lib/dmao/ingesters.rb
@@ -4,13 +4,16 @@ module DMAO
     ALL = []
     DETAILS = {}
     ORG_INGESTERS = {}
+    FILE_INGESTERS = []
 
-    def self.register name, display_name, version, type, ingester
+    def self.register name, display_name, version, type, ingester, ingester_type=nil
 
-      details = { name: name, display_name: display_name, version: version, type: type }
+      details = { name: name, display_name: display_name, version: version, type: type, ingester_type: ingester_type }
 
       ALL.insert(-1, name)
       DETAILS[name] = details
+
+      FILE_INGESTERS.insert(-1, name) if ingester_type == :file
 
       ORG_INGESTERS[name] = ingester if type == :organisation
 
--- test/lib/dmao/ingesters_test.rb
+++ test/lib/dmao/ingesters_test.rb
@@ -11,7 +11,8 @@ module DMAO
           name: :ingester_name,
           display_name: "INGESTER Name",
           version: 0.1,
-          type: :organisation
+          type: :organisation,
+          ingester_type: nil
       }
 
       assert_equal 1, DMAO::Ingesters::ALL.select { |v| v == :ingester_name }.size
@@ -33,7 +34,8 @@ module DMAO
           name: :ingester_name,
           display_name: "INGESTER Name",
           version: 0.1,
-          type: :testing
+          type: :testing,
+          ingester_type: nil
       }
 
       assert_equal 1, DMAO::Ingesters::ALL.select { |v| v == :ingester_name }.size
@@ -42,6 +44,30 @@ module DMAO
 
       DMAO::Ingesters::ALL.delete(:ingester_name)
       DMAO::Ingesters::DETAILS.delete(:ingester_name)
+
+    end
+
+    test 'add new ingester with organisation type and ingester type as file' do
+
+      DMAO::Ingesters.register(:ingester_name, "INGESTER Name", 0.1, :organisation, String, :file)
+
+      details_hash = {
+          name: :ingester_name,
+          display_name: "INGESTER Name",
+          version: 0.1,
+          type: :organisation,
+          ingester_type: :file
+      }
+
+      assert_equal 1, DMAO::Ingesters::ALL.select { |v| v == :ingester_name }.size
+      assert_equal 1, DMAO::Ingesters::ORG_INGESTERS.select { |v| v == :ingester_name }.size
+      assert_equal details_hash, DMAO::Ingesters::DETAILS[:ingester_name]
+      assert_equal 1, DMAO::Ingesters::FILE_INGESTERS.select { |v| v == :ingester_name }.size
+
+      DMAO::Ingesters::ALL.delete(:ingester_name)
+      DMAO::Ingesters::DETAILS.delete(:ingester_name)
+      DMAO::Ingesters::ORG_INGESTERS.delete(:ingester_name)
+      DMAO::Ingesters::FILE_INGESTERS.delete(:ingester_name)
 
     end
 
