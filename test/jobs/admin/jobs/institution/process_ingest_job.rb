d4570a93c2f942928bae70aefbbffab3a238160d
Stephen Robinson <stephen@stephen-robinson.co.uk>
2016-09-29 16:01:28 +0100

    Add process ingest job job

---
+++ test/jobs/admin/jobs/institution/process_ingest_job.rb
@@ -0,0 +1,25 @@
+require 'test_helper'
+
+class Admin::Jobs::Institution::ProcessIngestJobTest < ActiveJob::TestCase
+
+  def setup
+    @ingest_job = ingest_jobs(:ingest)
+  end
+
+  test 'should get ingest job from db for specified id' do
+
+    IngestJob.expects(:find).with(@ingest_job.id).once
+
+    ::Admin::Jobs::Institution::ProcessIngestJob.perform_now @ingest_job.id
+
+  end
+
+  test 'should log error if ingest job id cannot be found' do
+
+    Rails.logger.expects(:error).once
+
+    ::Admin::Jobs::Institution::ProcessIngestJob.perform_now 0
+
+  end
+
+end
