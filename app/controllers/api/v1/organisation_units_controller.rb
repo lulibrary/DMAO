de5af0f7c2a8bb9e63e6f11b6bb24af71adedfdf
Stephen Robinson <stephen@stephen-robinson.co.uk>
2016-10-11 12:28:43 +0100

    Add update organisation unit api v1 endpoint

--- app/controllers/api/v1/organisation_units_controller.rb
+++ app/controllers/api/v1/organisation_units_controller.rb
@@ -29,6 +29,66 @@ module Api
 
       end
 
+      def update
+
+        begin
+
+          institution = Institution.find params[:institution_id]
+
+          Institution.current_id = institution.id
+
+        rescue ActiveRecord::RecordNotFound
+          return render json: {
+              errors: {
+                  institution_id: "Institution not found for institution id #{params[:institution_id]}"
+              }
+          }, status: :not_found
+        end
+
+        begin
+
+          organisation_unit = Institution::OrganisationUnit.find params[:id]
+
+        rescue ActiveRecord::RecordNotFound
+
+          return render json: {
+              errors: {
+                  organisation_unit: "Organisation unit not found for organisation unit id #{params[:id]}"
+              }
+          }, status: :not_found
+
+        end
+
+        if params[:parent_uuid].present?
+
+          begin
+
+            parent_organisation_unit = Institution::OrganisationUnit.find params[:parent_uuid]
+
+          rescue ActiveRecord::RecordNotFound
+
+            return render json: {
+                errors: {
+                    parent_uuid: "Organisation unit not found for parent uuid #{params[:parent_uuid]}"
+                }
+            }, status: :unprocessable_entity
+
+          end
+
+          organisation_unit.parent = parent_organisation_unit
+
+        end
+
+        if organisation_unit.update(organisation_unit_params)
+          render json: organisation_unit, serializer: Api::V1::OrganisationUnitSerializer, status: :ok
+        else
+          render json: {
+              errors: organisation_unit.errors
+          }, status: :unprocessable_entity
+        end
+
+      end
+
       private
 
       def organisation_unit_params
